9fc04c033d344fd4fa15509b60e0348ba8f4a60a
ozlem-demir <ozlem.demi10@gmail.com>
2025-05-21 21:22:29 +0300

    Local changes before pull

--- account/settings.py
+++ account/settings.py
@@ -89,18 +89,22 @@ WSGI_APPLICATION = 'socialnetwork.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'eaglespace_db',
-        'USER': 'postgres',
-        'PASSWORD': 'ELeuFiVybBR7jn2161S68fIAnEmLWLsM',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        
-    }
-}
+#DATABASES = {
+#   'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'eaglespace_db',
+#        'USER': 'postgres',
+#        'PASSWORD': 'ELeuFiVybBR7jn2161S68fIAnEmLWLsM',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#        
+#    }
+#}
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
